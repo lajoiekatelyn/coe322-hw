2910e474851ed0fc990ad2cba7fcfe9907490116
klajoie <klajoie@login-coe332.tacc.utexas.edu>
2023-01-21 13:42:27 -0600

    Updated script01.py to get the correct answer.

--- homework01/script01.py
+++ homework01/script01.py
@@ -1,13 +1,30 @@
+# script to find the five longest words in the words file
+
+longest_words = ['', '', '', '', '']
+
 with open('/usr/share/dict/words', 'r') as f:
-    words = f.read().splitlines()
 
-longest_words = words[:5]
+    for word in f:
+        len_word = len(word.strip('\n'))
+        for i in range(5):
+            len_long_word = len(longest_words[i].strip('\n'))
+            if len_word > len_long_word:
+                longest_words.insert(i, word)
+                longest_words.pop(-1)
+                break
+            if len_word == len_long_word:
+                list = [word, longest_words[i]]
+                list.sort()
+                longest_words.pop(i)
+                longest_words.insert(i, list[1])
+                longest_words.insert(i, list[0])
+                longest_words.pop(-1)
+                break
 
-for word in words:
-    for i in range(len(longest_words)):
-        if len(word) > len(longest_words[i]):
-            longest_words[i] = word
-            break
-        
+print('\n\n\nlongest words start here\n')
 for word in longest_words:
-    print(f'{word}\n')
+    print(word.strip('\n'))
+
+
+
+
