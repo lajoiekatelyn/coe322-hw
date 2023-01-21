91a53b133f63e22fcea57edd5e7db8f73524881c
klajoie <klajoie@login-coe332.tacc.utexas.edu>
2023-01-21 13:52:44 -0600

    Added descriptive comments to script.

--- homework01/script02.py
+++ homework01/script02.py
@@ -1,6 +1,7 @@
 import names
 
 
+# generate 5 full names with character lengths of 8 (excluding spaces)
 i = 0
 while (i < 5):
     name = names.get_full_name()
--- homework01/script03.py
+++ homework01/script03.py
@@ -4,10 +4,11 @@ import names
 def len_name(name):
     return len(name) - 1
 
-
+# generate list of five names
 name_list = []
 for i in range(5):
     name_list.append(names.get_full_name())
 
+# print list of names and thier corresponding lengths(omitting the space character)
 for name in name_list:
     print(name, len_name(name), '\n')
