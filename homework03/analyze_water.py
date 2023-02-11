c0b889e8495f936e8fe479cd18b31eefc9d58568
klajoie <klajoie@login-coe332.tacc.utexas.edu>
2023-02-11 10:01:35 -0600

    Updated analyze_water.py to include docstrings and fine-tune output.

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -7,10 +7,29 @@ d = 0.02
 
 
 def calculate_turbidity(a0:float, I90:float) -> float:
+    """
+    Given a calibration constant and detector current, this function calculates the turbidity of water.
+
+    Arguments:
+        a0 (float): calibration constant
+        T0 (float): ninety degree detector current
+    Returns:
+        T (float): turbidity of the input data points in NTU
+    """
+
     return a0 * I90
 
 
 def calculate_time_to_Ts(T0:float) -> float:
+    """
+    This function calculate the time it takes in hours for a water sample to fall to the turbidity threshold.
+
+    Arguments:
+        T0 (float): current turbidity in NTU
+    Returns:
+        b (float): time it takes for the turbidity to fall to the threshold (1.0 NTU) in hours
+    """
+
     if ( T0 < Ts ):
         return 0.0
     else:
@@ -34,9 +53,9 @@ def main():
     print(f'Average turbidity based on most recent five measurements = {Tavg} NTU')
 
     if ( Tavg < Ts ):
-        print('Info: Turbidity is below threshold for safe use')
+        print('INFO: Turbidity is below threshold for safe use.')
     else:
-        print('Warning: Turbidity is above threshold for safe use')
+        print('WARNING: Turbidity is ABOVE threshold for safe use!')
 
     time_to_Ts = calculate_time_to_Ts(Tavg)
     print(f'Minimum time required to return below a safe threshold = {time_to_Ts} hours')
