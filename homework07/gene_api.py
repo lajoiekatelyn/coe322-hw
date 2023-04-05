bf4b779d8474a382a0cf9921739042116077bcbc
Ubuntu <ubuntu@klajoie-vm.js2local>
2023-04-05 16:17:45 +0000

    Updated Redis client for k8s usage.

--- homework07/gene_api.py
+++ homework07/gene_api.py
@@ -14,7 +14,7 @@ def get_redis_client():
     Returns
         redis_database (redis.client.Redis): Redis client
     """
-    return redis.Redis(host='redis-db', port=6379, db=0, decode_responses=True)
+    return redis.Redis(host='klajoie-test-redis-service', port=6379, db=0, decode_responses=True)
 
 rd = get_redis_client()
 
